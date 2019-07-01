1ac922364e90941f0eca3fce10bd9f2ba5a4cea4
stsega <stsega@bk.ru>
2019-07-01 16:27:53 -0400

    проработал JS урок по bind();

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let lang = 'Java';
+let prog = 'coding';
+
+var obj =
+     {
+        lang:"JavaScript",
+        prog:"programming",
+        language:function()
+        {
+            document.writeln(lang+ ' is a popular '+prog+' language.');
+        }
+    };
+
+
+obj.language();
+obj.language();
+
